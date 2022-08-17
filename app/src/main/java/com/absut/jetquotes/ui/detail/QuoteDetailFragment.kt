18b8f29ca8a757876f0afd30b27e322e2f6d7a77
Akshay <akshaynighot73@gmail.com>
2022-08-17 18:28:03 +0530

    save quote as image

--- app/src/main/java/com/absut/jetquotes/ui/detail/QuoteDetailFragment.kt
+++ app/src/main/java/com/absut/jetquotes/ui/detail/QuoteDetailFragment.kt
@@ -1,23 +1,24 @@
 package com.absut.jetquotes.ui.detail
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
+import android.content.*
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.absut.jetquotes.BuildConfig
 import com.absut.jetquotes.R
 import com.absut.jetquotes.databinding.FragmentQuoteDetailBinding
-import com.absut.jetquotes.databinding.FragmentQuotesBinding
 import com.absut.jetquotes.ui.viewmodel.QuoteViewModel
 import com.google.android.material.snackbar.Snackbar
+import java.io.File
+
 
 class QuoteDetailFragment : Fragment(R.layout.fragment_quote_detail) {
 
@@ -27,7 +28,7 @@ class QuoteDetailFragment : Fragment(R.layout.fragment_quote_detail) {
     private val args by navArgs<QuoteDetailFragmentArgs>()
     private val viewModel by activityViewModels<QuoteViewModel>()
 
-    private var isFavorite:Boolean = false
+    private var isFavorite: Boolean = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,21 +49,26 @@ class QuoteDetailFragment : Fragment(R.layout.fragment_quote_detail) {
                     args.quote.isFavorite = true
                 }
                 setFavoriteIcon()
-               /* val resultMsg = if (isFavorite) "Added to favorite" else "Removed from favorite"
-                Snackbar.make(view, resultMsg, Snackbar.LENGTH_SHORT)
-                    .setAnchorView(binding.constraintLayout)
-                    .show()*/
             }
+
             save.setOnClickListener {
-                Snackbar.make(view, "Quote saved", Snackbar.LENGTH_SHORT)
-                    .setAnchorView(binding.constraintLayout)
-                    .show()
+                val bitmap = getBitmapFromView(binding.quoteCard)
+                val uri = createImageUri(args.quote.author)
+                storeBitmap(bitmap, uri)
             }
+
             share.setOnClickListener {
-                Snackbar.make(view, "Share clicked", Snackbar.LENGTH_SHORT)
-                    .setAnchorView(binding.constraintLayout)
-                    .show()
+                val intent = Intent(Intent.ACTION_SEND)
+                    .setType("text/plain")
+                    .putExtra(
+                        Intent.EXTRA_TEXT,
+                        getQuoteCopy(args.quote.content, args.quote.author)
+                    )
+                if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+                    startActivity(Intent.createChooser(intent, "Share using"))
+                }
             }
+
             copy.setOnClickListener {
                 requireActivity().copyToClipboard(
                     "quote",
@@ -97,6 +103,49 @@ class QuoteDetailFragment : Fragment(R.layout.fragment_quote_detail) {
             )
             isFavorite = false
         }
+    }
+
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bg = view.background
+        bg.draw(canvas)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    private fun createImageUri(author: String): Uri {
+        val image = File(
+            requireActivity().applicationContext.filesDir,
+            "quote_by_${author.replace(" ", "").lowercase()}"
+        )
+        return FileProvider.getUriForFile(
+            requireActivity().applicationContext,
+            "${BuildConfig.APPLICATION_ID}.fileprovider",
+            image
+        )
+    }
+
+    private fun storeBitmap(bitmap: Bitmap, imageUri: Uri) {
+        val outputStream = requireActivity().applicationContext.contentResolver.openOutputStream(imageUri)
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream)
+        outputStream!!.close()
+
+        Snackbar.make(binding.constraintLayout, "Quote saved", Snackbar.LENGTH_LONG)
+            .setAnchorView(binding.constraintLayout)
+            .setAction("Open") {
+                openScreenshot(imageUri)
+            }
+            .show()
+    }
+
+
+    private fun openScreenshot(uri: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW)
+            .setDataAndType(uri, "image/*")
+            .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        startActivity(intent)
     }
 
     private fun Context.copyToClipboard(clipLabel: String, text: CharSequence) {
